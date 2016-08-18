b20048386fde472d84c84ced8f49e9bacde05351
alutman <alexander.lutman@gmail.com>
2016-08-18 21:15:09 +1000

    Abstracted out base dir for unit test paths

--- MBINCompilerTests/UnitTests.cs
+++ MBINCompilerTests/UnitTests.cs
@@ -1,31 +1,35 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MBINCompiler;
+using System.IO;
 
 namespace MBINCompilerTests
 {
     [TestClass]
     public class UnitTests
     {
+        String baseDir = "C:/NMS";
+
         [TestMethod]
         public void should_successfully_decompile()
         {
             var tests = new string[] // all of these should decompile fine
             {
-                @"C:\NMS\METADATA\UI\HUD\SHIPHUD.MBIN", 
-                @"C:\NMS\METADATA\ENTITLEMENTS\GCENTITLEMENTREWARDTABLE.MBIN",
-                @"C:\NMS\SCENES\DEMOS\PS4TEST\MAINSETTINGSPS4TEST.MBIN",
-                @"C:\NMS\METADATA\REALITY\TABLES\NMS_REALITY_GCTECHNOLOGYTABLE.MBIN",
-                @"C:\NMS\METADATA\SIMULATION\SOLARSYSTEM\WEATHER\CLEARWEATHER.MBIN",
-                @"C:\NMS\METADATA\SIMULATION\SOLARSYSTEM\WEATHER\SKYSETTINGS\DAYSKYCOLOURS.MBIN",
-                @"C:\NMS\LANGUAGE\NMS_LOC1_ENGLISH.MBIN",
-                @"C:\NMS\METADATA\REALITY\TABLES\NMS_DIALOG_GCALIENSPEECHTABLE.MBIN",
-                @"C:\NMS\METADATA\REALITY\TABLES\NMS_DIALOG_GCALIENPUZZLETABLE.MBIN"
+                "METADATA/UI/HUD/SHIPHUD.MBIN", 
+                "METADATA/ENTITLEMENTS/GCENTITLEMENTREWARDTABLE.MBIN",
+                "SCENES/DEMOS/PS4TEST/MAINSETTINGSPS4TEST.MBIN",
+                "METADATA/REALITY/TABLES/NMS_REALITY_GCTECHNOLOGYTABLE.MBIN",
+                "METADATA/SIMULATION/SOLARSYSTEM/WEATHER/CLEARWEATHER.MBIN",
+                "METADATA/SIMULATION/SOLARSYSTEM/WEATHER/SKYSETTINGS/DAYSKYCOLOURS.MBIN",
+                "LANGUAGE/NMS_LOC1_ENGLISH.MBIN",
+                "METADATA/REALITY/TABLES/NMS_DIALOG_GCALIENSPEECHTABLE.MBIN",
+                "METADATA/REALITY/TABLES/NMS_DIALOG_GCALIENPUZZLETABLE.MBIN"
             };
 
             foreach (var test in tests)
             {
-                var file = new MBINFile(test);
+                String fullPath = Path.Combine(baseDir, test);
+                var file = new MBINFile(fullPath);
                 file.Load();
                 Assert.IsNotNull(file.SerializeToXML(), test + " serialization was null");
             }
