f59d557caf6525e066bd694433f4260f9f3adb30
alutman <alexander.lutman@gmail.com>
2016-08-18 20:28:40 +1000

    Moved tests to UnitTest project

---
+++ MBINCompilerTests/UnitTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MBINCompiler;
+
+namespace MBINCompilerTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void should_successfully_decompile()
+        {
+            var tests = new string[] // all of these should decompile fine
+            {
+                @"C:\NMS\METADATA\UI\HUD\SHIPHUD.MBIN", 
+                @"C:\NMS\METADATA\ENTITLEMENTS\GCENTITLEMENTREWARDTABLE.MBIN",
+                @"C:\NMS\SCENES\DEMOS\PS4TEST\MAINSETTINGSPS4TEST.MBIN",
+                @"C:\NMS\METADATA\REALITY\TABLES\NMS_REALITY_GCTECHNOLOGYTABLE.MBIN",
+                @"C:\NMS\METADATA\SIMULATION\SOLARSYSTEM\WEATHER\CLEARWEATHER.MBIN",
+                @"C:\NMS\METADATA\SIMULATION\SOLARSYSTEM\WEATHER\SKYSETTINGS\DAYSKYCOLOURS.MBIN",
+                @"C:\NMS\LANGUAGE\NMS_LOC1_ENGLISH.MBIN",
+                @"C:\NMS\METADATA\REALITY\TABLES\NMS_DIALOG_GCALIENSPEECHTABLE.MBIN",
+                @"C:\NMS\METADATA\REALITY\TABLES\NMS_DIALOG_GCALIENPUZZLETABLE.MBIN"
+            };
+
+            foreach (var test in tests)
+            {
+                var file2 = new MBINFile(test);
+                file2.Load();
+                var wew = file2.SerializeToXML();
+            }
+        }
+    }
+}
