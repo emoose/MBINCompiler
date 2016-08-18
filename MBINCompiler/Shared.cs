24dad968b55e6feb94a0c11f81659ca31891f62e
BananaSft <Banana@banana.software>
2016-08-18 18:56:48 +0200

    Split up MBINStructs.cs in seperate files

--- MBINCompiler/Shared.cs
+++ MBINCompiler/Shared.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using System.Reflection;
+using MBINCompiler.Models.Structs;
+using MBINCompiler.Models;
 
 namespace MBINCompiler
 {
