24d7b28c7ea9f5383e9d84f2c5d538dbbd7f7124
Naomi <naomichan@users.noreply.github.com>
2016-08-20 21:52:07 +0100

    Implement GcProductTable, GcProductData, and GcItemPriceModifiers

---
+++ MBINCompiler/Models/Structs/GcRarity.cs
@@ -0,0 +1,12 @@
+﻿namespace MBINCompiler.Models.Structs
+{
+    public class GcRarity : NMSTemplate
+    {
+        public int Rarity;
+
+        public string[] RarityValues()
+        {
+            return new[] { "Common", "Uncommon", "Rare", "Extraordinary", "None" };
+        }
+    }
+}
---
+++ MBINCompiler/Models/Structs/TkModelResource.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MBINCompiler.Models.Structs
+{
+    public class TkModelResource : NMSTemplate
+    {
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x80)]
+        public string Filename;
+        public int EmptyNode1;
+    }
+}
