6d1aff9c8ba4b8d6c4ac104d622d22f1d2629676
BananaSft <Banana@banana.software>
2016-08-21 20:43:12 +0200

    Added ReadTemplateFromString/Stream methods to EXmlFile

--- MBINCompiler/EXmlFile.cs
+++ MBINCompiler/EXmlFile.cs
@@ -6,21 +6,37 @@ using MBINCompiler.Models;
 
 namespace MBINCompiler
 {
-    internal static class EXmlFile
+    public static class EXmlFile
     {
         private static readonly XmlSerializer Serializer = new XmlSerializer(typeof(EXmlData));
         private static readonly XmlSerializerNamespaces Namespaces = new XmlSerializerNamespaces(new[] { new XmlQualifiedName("", "") });
-        
+
         public static NMSTemplate ReadTemplate(string filePath)
         {
             using (var input = File.OpenRead(filePath))
-            using (var reader = XmlReader.Create(input))
-            {
-                EXmlData root = (EXmlData)Serializer.Deserialize(reader);
-                NMSTemplate rootTemplate = NMSTemplate.DeserializeEXml(root);
-                return rootTemplate;
-            }
+                return ReadTemplateFromStream(input);
         }
+
+        public static NMSTemplate ReadTemplateFromString(string xml)
+        {
+            using (var reader = new StringReader(xml))
+            using (var xmlReader = XmlReader.Create(reader))
+                return ReadTemplateFromXmlReader(xmlReader);
+        }
+
+        public static NMSTemplate ReadTemplateFromStream(Stream input)
+        {
+            using (var reader = XmlReader.Create(input))
+                return ReadTemplateFromXmlReader(reader);
+        }
+
+        private static NMSTemplate ReadTemplateFromXmlReader(XmlReader reader)
+        {
+            EXmlData root = (EXmlData)Serializer.Deserialize(reader);
+            NMSTemplate rootTemplate = NMSTemplate.DeserializeEXml(root);
+            return rootTemplate;
+        }
+
 
         public static string WriteTemplate(NMSTemplate template)
         {
