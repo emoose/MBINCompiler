9a536b4e7dde59d5491b0c29280688eea395074b
emoose <abc@cock.li>
2016-08-22 21:20:24 +0100

    GcCreatureFilenameTable + tests

---
+++ MBINCompiler/Models/Structs/GcCreatureFilename.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MBINCompiler.Models.Structs
+{
+    public class GcCreatureFilename : NMSTemplate
+    {
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x10)]
+        public string ID;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x80)]
+        public string Filename;
+    }
+}
