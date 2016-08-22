0f0b269a7c6f0e294abf392f57328f4a4a6d8560
emoose <abc@cock.li>
2016-08-22 23:53:45 +0100

    Add TkControllerList & TkControllerSpecification

---
+++ MBINCompiler/Models/Structs/TkButtonPathMapping.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace MBINCompiler.Models.Structs
+{
+    public class TkButtonPathMapping : NMSTemplate
+    {
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x10)]
+        public string Id;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x80)]
+        public string Path;
+    }
+}
