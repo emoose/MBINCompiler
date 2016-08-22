3f347c61cedf44707232ebbaf53b94e4744ba1e5
SirBarclay <barclaycsgo@gmail.com>
2016-08-22 09:33:02 +0100

    fixed public

--- MBINCompiler/Models/Structs/GcRewardTableItemList.cs
+++ MBINCompiler/Models/Structs/GcRewardTableItemList.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 namespace MBINCompiler.Models.Structs
 {
-    class GcRewardTableItemList
+    public class GcRewardTableItemList
     {
         public bool EntitlementLinked;
 
