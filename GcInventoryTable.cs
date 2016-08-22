e56c723b4e25038abf563fc31fb47a9b47dc6426
SirBarclay <barclaycsgo@gmail.com>
2016-08-22 07:36:08 +0100

    sadf

--- GcInventoryTable.cs
+++
@@ -1,9 +0,0 @@
-using System.Runtime.InteropServices;
-
-namespace MBINCompiler.Models.Structs {
-
-    public class cGcInventoryTable : NMSTemplate {
-    	public List<cGcInventoryTableEntry> Table;
-    }
-
-}
