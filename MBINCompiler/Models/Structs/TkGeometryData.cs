f9fa820cfd5f9a0f8869293624d54b8edd3ceac4
Elementia <elementiafive@gmail.com>
2016-08-26 15:26:25 +0200

    Implements VertexStream, finishing TkGeometryData

--- MBINCompiler/Models/Structs/TkGeometryData.cs
+++ MBINCompiler/Models/Structs/TkGeometryData.cs
@@ -29,8 +29,8 @@ namespace MBINCompiler.Models.Structs
 		public TkVertexLayout SmallVertexLayout;
 		
 		public List<int> IndexBuffer;
-		/*public List<int> VertexStream;
-		public List<int> SmallVertexStream;*/
+		public List<float> VertexStream;
+		public List<float> SmallVertexStream;
 
         public override object CustomDeserialize(BinaryReader reader, Type field, NMSAttribute settings, long templatePosition, FieldInfo fieldInfo)
         {
@@ -68,12 +68,84 @@ namespace MBINCompiler.Models.Structs
 
                     return indices;
                 case "VertexStream":
-                    return null;
                 case "SmallVertexStream":
-                    return null;
+                    listPosition = reader.BaseStream.Position;
+                    Debug.WriteLine($"DeserializeList start 0x{listPosition:X}");
+
+                    listStartOffset = reader.ReadInt64();
+                    numEntries = reader.ReadInt32();
+                    listMagic = reader.ReadUInt32();
+                    if (listMagic != 0xAAAAAA01 && listMagic != 1)
+                        throw new Exception($"Invalid list read, magic 0x{listMagic:X} expected 0xAAAAAA01 / 0x1");
+
+                    listEndPosition = reader.BaseStream.Position;
+
+                    reader.BaseStream.Position = listPosition + listStartOffset;
+                    List<float> vertices = new List<float>();
+                    while (reader.BaseStream.Position < (listPosition + listStartOffset + numEntries))
+                    {
+                        vertices.Add((float)Extensions.ReadHalfLittle(reader));
+                    }
+
+                    reader.BaseStream.Position = listEndPosition;
+                    reader.Align(0x8, 0);
+
+                    return vertices;
                 default:
                     return null;
             }
+        }
+    }
+
+    // Thank you to HugoPeters for this class :
+    // https://github.com/HugoPeters/NMS-Tools/blob/master/NMS-View/Source/NMSView/NMSView.Windows/HalfHelper.cs
+    public static class Extensions
+    {
+        public static float ReadHalfLittle(this BinaryReader binaryReader)
+        {
+            UInt16 u = binaryReader.ReadUInt16();
+            int sign = (u >> 15) & 0x00000001;
+            int exp = (u >> 10) & 0x0000001F;
+            int mant = u & 0x000003FF;
+
+            exp = exp + (127 - 15);
+
+            int i = (sign << 31) | (exp << 23) | (mant << 13);
+            byte[] buff = BitConverter.GetBytes(i);
+
+            return BitConverter.ToSingle(buff, 0);
+        }
+
+        public static float ToFloat(UInt16 u)
+        {
+            // UInt16 u = binaryReader.ReadUInt16();
+            int sign = (u >> 15) & 0x00000001;
+            int exp = (u >> 10) & 0x0000001F;
+            int mant = u & 0x000003FF;
+
+            exp = exp + (127 - 15);
+
+            int i = (sign << 31) | (exp << 23) | (mant << 13);
+            byte[] buff = BitConverter.GetBytes(i);
+
+            return BitConverter.ToSingle(buff, 0);
+        }
+
+        public static UInt16 ToHalf(this float f)
+        {
+            byte[] bytes = BitConverter.GetBytes((double)f);
+            ulong bits = BitConverter.ToUInt64(bytes, 0);
+            ulong exponent = bits & 0x7ff0000000000000L;
+            ulong mantissa = bits & 0x000fffffffffffffL;
+            ulong sign = bits & 0x8000000000000000L;
+            int placement = (int)((exponent >> 52) - 1023);
+            if (placement > 15 || placement < -14)
+                return ToHalf(-1.0f);
+
+            UInt16 exponentBits = (UInt16)((15 + placement) << 10);
+            UInt16 mantissaBits = (UInt16)(mantissa >> 42);
+            UInt16 signBits = (UInt16)(sign >> 48);
+            return (UInt16)(exponentBits | mantissaBits | signBits);
         }
     }
 }
