643d50b2eee3a2add3eafb997284ed9af61cf8c7
emoose <abc@cock.li>
2016-08-27 01:25:47 +0100

    Add skeletons for GcPlacementGlobals, GcSmokeTestOptions & GcTerrainGlobals Moved GcPlanetData to /Models/Structs/Unfinished/

---
+++ MBINCompiler/Models/Structs/Unfinished/GcTerrainGlobals.cs
@@ -0,0 +1,53 @@
+﻿namespace MBINCompiler.Models.Structs
+{
+    public class GcTerrainGlobals : NMSTemplate
+    {
+        public bool Unknown0; // if set, skips a lot of code
+        public bool Unknown1; // unused?
+        public bool Unknown2; // if unset, does some more calcs
+        public bool Unknown3; // unused
+
+        public int Unknown4; // planet gen related
+        public int Unknown8; // planet gen related
+        public int UnknownC; // planet gen related
+
+        public float Unknown10; // unused?
+        public float Unknown14; // unused?
+        public float Unknown18;
+        public float Unknown1C;
+
+        public float Unknown20;
+        public float Unknown24;
+        public float Unknown28;
+        public int Unknown2C;
+
+        public bool Unknown30;
+        public bool Unknown31; // unused
+        public bool Unknown32; // unused
+        public bool Unknown33; // unused
+        public int Unknown34;
+        public int Unknown38;
+        public bool Unknown3C; // if set, overrides some values with the values from 34 / 40 / 4C
+        public bool Unknown3D; // unused
+        public bool Unknown3E; // unused
+        public bool Unknown3F; // unused
+
+        public int Unknown40;
+        public int Unknown44;
+        public bool Unknown48;
+        public bool Unknown49; // unused
+        public bool Unknown4A; // unused
+        public bool Unknown4B; // unused
+        public float Unknown4C;
+
+        public float Unknown50; // unused?
+        public float Unknown54; // unused?
+        public float Unknown58; // unused?
+        public float Unknown5C;
+
+        public float Unknown60;
+        public float Unknown64;
+        public float Unknown68;
+        public float Unknown6C;
+    }
+}
