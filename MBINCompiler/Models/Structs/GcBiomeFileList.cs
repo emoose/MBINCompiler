4ff906a3a4367c8004f36ee51310db7b85ead6c8
emoose <abc@cock.li>
2016-08-27 03:31:18 +0100

    GcBiomeFileList

---
+++ MBINCompiler/Models/Structs/GcBiomeFileList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MBINCompiler.Models.Structs
+{
+    public class GcExternalObjectFileList : NMSTemplate
+    {
+        [NMS(Size = 0x10)]
+        public string Id;
+
+        public bool OnlyOnExtremeWeather;
+        public bool OnlyOnExtremeSentinels;
+        public bool OnlyOnDeepWater;
+        public bool NotOnExtremePlanets;
+        public bool NotOnStartPlanets;
+
+        public GcBiomeType Biome;
+
+        public float ProbabilityOfBeingActive;
+        public int MinFilesToChoose;
+        public int MaxFilesToChoose;
+
+        public List<GcExternalObjectListOptions> ExternalObjectFiles;
+
+    }
+    public class GcBiomeFileList : NMSTemplate
+    {
+        [NMS(Size = 9)]
+        public NMSString0x80[] BiomeFiles;
+
+        public List<GcExternalObjectListOptions> CommonExternalObjectLists;
+        public List<GcExternalObjectFileList> OptionalExternalObjectLists;
+    }
+}
