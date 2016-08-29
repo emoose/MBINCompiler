a488d2238c45569e7633515eff839edb098c03d9
emoose <abc@cock.li>
2016-08-30 00:59:56 +0100

    Add TkInputFrameArray, add GcCreatureRoleFilenameTable to tests

---
+++ MBINCompiler/Models/Structs/TkInputFrame.cs
@@ -0,0 +1,14 @@
+﻿namespace MBINCompiler.Models.Structs
+{
+    public class TkInputFrame : NMSTemplate // 0x1C bytes
+    {
+        public Vector2f LeftStick;
+        public Vector2f RightStick;
+        public float LeftTrigger;
+        public float RightTrigger;
+        public short Buttons;
+
+        [NMS(Size = 2, Ignore = true)]
+        public byte[] Padding1A;
+    }
+}
---
+++ MBINCompiler/Models/Structs/TkInputFrameArray.cs
@@ -0,0 +1,8 @@
+﻿namespace MBINCompiler.Models.Structs
+{
+    public class TkInputFrameArray : NMSTemplate
+    {
+        [NMS(Size = 0x4E20)] // INPUTTEST.MBIN has a different count of these? uncertain because we can't tell the MBIN header size..
+        public TkInputFrame[] Array;
+    }
+}
