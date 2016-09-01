e4d6c66c74b6d4605c9cb4a404b9c8b83a153d4a
BananaSft <banana@banana.software>
2016-09-01 15:34:33 +0200

    Refactor NMSAttribute
    Changed properties to autoproperties
    Removed obsolete contructor

--- MBINCompiler/Models/NMSAttribute.cs
+++ MBINCompiler/Models/NMSAttribute.cs
@@ -1,72 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MBINCompiler.Models
 {
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.GenericParameter)]
     public class NMSAttribute : Attribute
     {
-        private int size;
-        public int Size
-        {
-          get
-          {
-              return size;
-          }
-          set
-          {
-              size = value;
-          }
-        }
-
-        private bool ignore;
-        public bool Ignore
-        {
-            get
-            {
-                return ignore;
-            }
-            set
-            {
-                ignore = value;
-            }
-        }
-
-        private object defaultValue;
-        public object DefaultValue
-        {
-            get
-            {
-                return defaultValue;
-            }
-            set
-            {
-                defaultValue = value;
-            }
-        }
-
-        private string[] enumValue;
-        public string[] EnumValue
-        {
-            get
-            {
-                return enumValue;
-            }
-            set
-            {
-                enumValue = value;
-            }
-        }
-
-        public NMSAttribute() 
-        {
-            size = 0;
-            ignore = false;
-            defaultValue = null;
-            enumValue = null;
-        }
+        public int Size { get; set; }
+        public bool Ignore { get; set; }
+        public object DefaultValue { get; set; }
+        public string[] EnumValue { get; set; }
     }
 }
