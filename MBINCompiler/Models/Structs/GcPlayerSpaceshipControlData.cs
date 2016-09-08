6f53d5813aba22cba4bc0577a3e4d4a075893cc8
emoose <abc@cock.li>
2016-09-08 19:02:04 +0100

    Generate GcSpaceshipGlobals template + map child templates

---
+++ MBINCompiler/Models/Structs/GcPlayerSpaceshipControlData.cs
@@ -0,0 +1,30 @@
+﻿namespace MBINCompiler.Models.Structs
+{
+    public class GcPlayerSpaceshipControlData : NMSTemplate
+    {
+        public GcPlayerSpaceshipEngineData SpaceEngine;
+        public GcPlayerSpaceshipEngineData PlanetEngine;
+
+        public float ExitHeightFactorMin;
+        public float ExitHeightFactorMax;
+        public float ExitHeightFactorPlungeMin;
+        public float ExitHeightFactorPlungeMax;
+        public float ExitAngleMin;
+        public float ExitAngleMax;
+
+        public TkCurveType ExitCurve;
+        public TkCurveType ExitDownCurve;
+
+        public float AngularFactor;
+        public float MaxTorque;
+        public float ShipPlanetBrakeMinSpeed;
+        public float ShipPlanetBrakeMaxSpeed;
+        public float ShipPlanetBrakeMinHeight;
+        public float ShipPlanetBrakeMaxHeight;
+        public float ShipPlanetBrakeForce;
+        public float ShipPlanetBrakeAlignMinTime;
+        public float ShipPlanetBrakeAlignMaxTime;
+
+        public float ShipMinHeightForce;
+    }
+}
---
+++ MBINCompiler/Models/Structs/GcPlayerSpaceshipEngineData.cs
@@ -0,0 +1,27 @@
+﻿namespace MBINCompiler.Models.Structs
+{
+    public class GcPlayerSpaceshipEngineData : NMSTemplate
+    {
+        public float ThrustForce;
+        public float MaxSpeed;
+        public float MinSpeed;
+        public float Falloff;
+        public float BoostThrustForce;
+        public float BoostMaxSpeed;
+        public float BoostFalloff;
+        public float BoostingTurnDamp;
+        public float DirectionBrakeMin;
+        public float DirectionBrake;
+        public float ReverseBrake;
+        public float OverspeedBrake;
+        public float LowSpeedTurnDamper;
+        public float TurnBrakeMin;
+        public float TurnBrakeMax;
+        public float TurnStrength;
+        public float RollAmount;
+        public float RollForce;
+        public float RollAutoTime;
+        public float BalanceTimeMin;
+        public float BalanceTimeMax;
+    }
+}
