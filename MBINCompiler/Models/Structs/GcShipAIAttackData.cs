132da8c71fceda4304b846f4279e249ac54fff9c
emoose <abc@cock.li>
2016-09-08 19:25:00 +0100

    Add skeleton for GcAISpaceshipGlobals + mapped child templates

---
+++ MBINCompiler/Models/Structs/GcShipAIAttackData.cs
@@ -0,0 +1,41 @@
+﻿namespace MBINCompiler.Models.Structs
+{
+    public class GcShipAIAttackData : NMSTemplate
+    {
+        public float AttackWeaponRange;
+        public float AttackAngle;
+        public float AttackShootWaitTime;
+        public float AttackShootTimeMin;
+        public float AttackShootTimeMax;
+        public float AttackReadyTime;
+        public float AttackMaxTime;
+        public float AttackApproachOffset;
+        public float AttackApproachMinRange;
+        public float AttackApproachMaxRange;
+        public float AttackTooCloseRange;
+        public float AttackFlybyOffset;
+        public float AttackBoostTimeToRange;
+        public float AttackBoostRange;
+        public float AttackBoostAngle;
+        public float AttackTurnMultiplier;
+        public float AttackFacingAimReducer;
+        public float AttackMaxPlanetHeight;
+        public int NumHitsBeforeBail;
+        public int NumHitsBeforeReposition;
+        public float FleeBrake;
+        public float FleeBoost;
+        public float FleeBrakeTime;
+        public float FleeRepositionTime;
+        public float FleeRepositionAngleMin;
+        public float FleeRepositionAngleMax;
+        public float FleeRepositionBoostTime;
+        public float FleeRange;
+        public float FleeMinTime;
+        public float FleeMaxTime;
+        public float AttackTargetMinRange;
+        public float AttackTargetMaxRange;
+        public float AttackTargetOffsetMin;
+        public float AttackTargetOffsetMax;
+        public float AttackTargetSwitchTargetTime;
+    }
+}
---
+++ MBINCompiler/Models/Structs/GcShipAIPlanetPatrolData.cs
@@ -0,0 +1,21 @@
+﻿namespace MBINCompiler.Models.Structs
+{
+    public class GcShipAIPlanetPatrolData : NMSTemplate
+    {
+        [NMS(Size = 0x10)]
+        public string Squad;
+
+        public float PlayerOffset;
+        public float PlayerFalloff;
+        public float PathOffset;
+        public float WaypointDistance;
+        public float PathSpeed;
+        public float AlongPathForce;
+        public float ToPathForce;
+        public float BrakeForce;
+        public float AlignForce;
+
+        [NMS(Size = 4, Ignore = true)]
+        public byte[] Padding34;
+    }
+}
