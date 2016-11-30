c75234701e5030bc67b7b084d0ead2abddbcab6f
Aaron Fisher <aaron@fishcodesign.com>
2016-11-30 00:18:13 -0500

    My first structs
    Pretty sure everything works except GcDestructableComponentData

--- MBINCompiler/Models/Structs/GcDestructableComponentData.cs
+++ MBINCompiler/Models/Structs/GcDestructableComponentData.cs
@@ -18,26 +18,29 @@ namespace MBINCompiler.Models.Structs
 
         [NMS(Size = 0x10)]
         /* 0x030 */ public string LootReward;
+
         /* 0x040 */ public int LootRewardAmountMin;
         /* 0x044 */ public int LootRewardAmountMax;
 
         /* 0x048 */ public List<GcSubstanceAmount> GivesSubstances;
         [NMS(Size = 0x10)]
         /* 0x058 */ public string GivesReward;
+		/* 0x068 */ public float HardModeSubstanceMultiplier;
 
-        /* 0x068 */ public bool RemoveModel;
-        /* 0x06C */ public TkModelResource DestroyedModel;
-        /* 0x0F0 */ public bool DestroyedModelUsesScale;
-        /* 0x0F4 */ public float DestroyForce;
-        /* 0x0F8 */ public float DestroyForceRadius;
-        /* 0x100 */ public List<NMSString0x10> DestroyEffectPoints;
-        /* 0x110 */ public float DestroyEffectTime;
+        /* 0x06C */ public bool RemoveModel;
+        /* 0x070 */ public TkModelResource DestroyedModel;
+        /* 0x0F4 */ public bool DestroyedModelUsesScale;
+        /* 0x0F8 */ public float DestroyForce;
+        /* 0x0FC */ public float DestroyForceRadius;
+		/* 0x100 */ public string DestroyEffect;
+        /* 0x110 */ public List<NMSString0x10> DestroyEffectPoints;
+        /* 0x120 */ public float DestroyEffectTime;
 
-        /* 0x114 */ public bool ShowInteract;
-        /* 0x118 */ public float ShowInteractRange;
-        /* 0x11C */ public bool GrenadeSingleHit;
+        /* 0x124 */ public bool ShowInteract;
+        /* 0x128 */ public float ShowInteractRange;
+        /* 0x12C */ public bool GrenadeSingleHit;
 
         [NMS(Size = 3, Ignore = true)]
-        /* 0x11D */ public byte[] Padding11D;
+        /* 0x12D */ public byte[] Padding12D;
     }
 }
