7479589f9d5ee6df1b19a9c067c8b2eb18fe6ac8
Aaron Fisher <aaron@fishcodesign.com>
2016-12-01 18:02:23 -0500

    GcBaseBuildingComponentData fixed

---
+++ MBINCompiler/Models/Structs/GcBaseBuildingComponentData.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace MBINCompiler.Models.Structs
+{
+    public class GcBaseBuildingComponentData : NMSTemplate
+    {
+		[NMS(Size = 0x10)]
+		/* 0x000 */ public string RegisterID;
+		/* 0x010 */ public GcPersistentBaseTypes Type;
+	}
+}
