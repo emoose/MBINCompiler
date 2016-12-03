1b82bdf8491eb5937a568f8f7ec8c5b29646bc1b
Aaron Fisher <aaron@fishcodesign.com>
2016-12-03 01:23:25 -0500

    More changes

---
+++ MBINCompiler/Models/Structs/Unused/GcBuildingClassification.cs
@@ -0,0 +1,11 @@
+﻿namespace MBINCompiler.Models.Structs
+{
+    public class GcBuildingClassification : NMSTemplate 
+    {
+		/* 0x000 */ public int BuildingClass;
+		public string[] ScanEffectTypeValues()
+		{
+			return new[] { "None", "TerrainResource", "Shelter", "Abandoned", "Terminal", "Shop", "Outpost", "Waypoint", "Beacon", "RadioTower" "Observatory", "Depot", "Factory", "Harvester", "Plaque", "Monolith", "Portal", "Ruin", "Debris", "DamagedMachine", "DistressSignal", "LandingPad", "Base"};
+		}
+    }
+}
---
+++ MBINCompiler/Models/Structs/Unused/GcScanEffectData.cs
@@ -0,0 +1,22 @@
+﻿namespace MBINCompiler.Models.Structs
+{
+    public class GcScanEffectData : NMSTemplate 
+    {
+		/* 0x000 */ public int ScanEffectType; // Not sure about calling this an int with the padding... maybe string?
+		public string[] ScanEffectTypeValues()
+		{
+			return new[] { "Building", "TargetShip", "Creature", "Ground", "Objects" };
+		}
+		[NMS(Size = 6, Ignore = true)]
+        public byte[] Padding4;
+
+		/* 0x010 */ public Colour Colour;
+		/* 0x020 */ public float BasecolourIntensity;
+		/* 0x024 */ public float ScanlinesSeparation;
+		/* 0x028 */ public float FresnelIntensity;
+		/* 0x02C */ public bool FixedUpAxis;
+		/* 0x02D */ public bool Transparent;
+		/* 0x030 */ public float FadeInTime;
+		/* 0x034 */ public float FadeOutTime;
+    }
+}
