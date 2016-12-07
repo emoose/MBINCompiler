2881a7b6771144752702a177290bccf04f1a73ca
monkeyman192 <monkey_man_192@yahoo.com.au>
2016-12-07 12:00:07 +1100

    small fix
    GcGalaxyMarkerTypes didn't sync properly

--- MBINCompiler/Models/Structs/GcGalaxyMarkerTypes.cs
+++ MBINCompiler/Models/Structs/GcGalaxyMarkerTypes.cs
@@ -1,7 +1,11 @@
 ﻿namespace MBINCompiler.Models.Structs
 {
+    public class GcGalaxyMarkerTypes : NMSTemplate
     {
+        public int GalaxyMarkerType;
+        public string[] GalaxyMarkerTypeValues()
         {
+            return new[] { "Home", "Waypoint", "Contact", "Blackhole", "AtlasStation", "Selection"};
         }
     }
 }
