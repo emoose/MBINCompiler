bf9acbfcb5b6ffc3a704d0f1859e962807ea7735
Gaticus Hax <gaticus.hax@gmail.com>
2018-07-08 18:11:19 -0700

    - renamed libMBIN-Shared/Common/Shared.cs to BinaryStreamExtensions.cs - renamed MBINCompilerTests/SharedTest.cs to WriteStringTest.cs - cleaned up code in WriteStringTest

---
+++ libMBIN/Source/Common/BinaryStreamExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
+
+namespace libMBIN {
+
+    internal static class BinaryStreamExtensions {
+
+        public static void Align( this BinaryReader reader, int alignBy, long templateStartOffset ) {
+            long offset = reader.BaseStream.Position - templateStartOffset;
+            long mod = offset % alignBy;
+            if (mod != 0) reader.BaseStream.Position += (alignBy - mod);
+        }
+
+        public static void Align( this BinaryWriter writer, int alignBy, long templateStartOffset ) {
+            long offset = writer.BaseStream.Position - templateStartOffset;
+            long mod = offset % alignBy;
+            if (mod != 0) writer.Write( new byte[alignBy - mod] ); // :)
+        }
+
+        public static string ReadString( this BinaryReader reader, Encoding encoding, int size, bool nullTerminated = false ) {
+            byte[] stringData = reader.ReadBytes( size );
+            string stringValue = encoding.GetString( stringData );
+            if (nullTerminated) {
+                int nullIndex = stringValue.IndexOf( "\0", StringComparison.Ordinal );
+                if (nullIndex >= 0) {
+                    stringValue = stringValue.Remove( nullIndex );
+                }
+            }
+
+            return stringValue;
+        }
+
+        public static void WriteString( this BinaryWriter writer, string str, Encoding encoding = null, int? size = null, bool nullTerminated = false ) {
+            encoding = encoding ?? Encoding.ASCII;
+            str = str ?? String.Empty;
+
+            int bufferSize = encoding.GetBytes( str ).Length;
+            if (size != null) bufferSize = size.Value;
+
+            int encodingSize = encoding.GetMaxByteCount( 1 );
+
+            if (nullTerminated && size == null) bufferSize += 1; // add space for null terminator
+
+            byte[] stringBytes = encoding.GetBytes( str );
+            Array.Resize( ref stringBytes, bufferSize );
+
+            if (nullTerminated && bufferSize > 0) {
+                var trimmedStringBytes = encoding.GetBytes( encoding.GetString( stringBytes ) );
+                if ( trimmedStringBytes.Length > stringBytes.Length ) {
+                    trimmedStringBytes = stringBytes; // .NET failed to reencode the trimmed string properly?
+                }
+
+                int nullOffset = trimmedStringBytes.Length;
+                if ( nullOffset == bufferSize )
+                    nullOffset -= 1;
+
+                stringBytes[nullOffset] = 0; // atm this might add a null in the middle of a character!
+            }
+
+            writer.Write( stringBytes );
+        }
+
+    }
+
+}
