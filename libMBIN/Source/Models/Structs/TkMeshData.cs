b363d57b5fc4394017b1acd3e04de25a78c3f4e1
monkeyman192 <monkey_man_192@yahoo.com.au>
2018-08-11 13:18:17 +1000

    fixed 3 globals for 1.55. Also modified how geometry.data files are decompiled. Very un-human friendly, but should facilitate the model reader well

--- libMBIN/Source/Models/Structs/TkMeshData.cs
+++ libMBIN/Source/Models/Structs/TkMeshData.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Reflection;
 
 namespace libMBIN.Models.Structs
 {
@@ -10,6 +15,32 @@ namespace libMBIN.Models.Structs
         /* 0x80 */ public ulong Hash;
         /* 0x88 */ public int VertexDataSize;
         /* 0x8C */ public int IndexDataSize;
-        ///* 0x90 */ public List<float> MeshDataStream;   // not really but yeah
+        /* 0x90 */ public byte[] MeshDataStream;
+
+        public override object CustomDeserialize(BinaryReader reader, Type field, NMSAttribute settings, long templatePosition, FieldInfo fieldInfo)
+        {
+            var fieldName = fieldInfo.Name;
+            switch (fieldName)
+            {
+                case nameof(MeshDataStream):
+                    long listPosition = reader.BaseStream.Position;
+                    //Debug.WriteLine($"TkGeometryData.CustomDeserialize({fieldName}) start 0x{listPosition:X}");
+
+                    long listStartOffset = reader.ReadInt64();
+                    int numEntries = reader.ReadInt32();
+                    uint listMagic = reader.ReadUInt32();
+                    if ((listMagic & 0xFF) != 1)
+                        throw new Exception($"Invalid list read, magic {listMagic:X8} expected xxxxxx01");
+                    long listEndPosition = reader.BaseStream.Position;
+
+                    reader.BaseStream.Position = listPosition + listStartOffset;
+                    byte[] data = new byte[numEntries];
+                    data = reader.ReadBytes(numEntries);
+                    reader.BaseStream.Position = listEndPosition;
+                    return data;
+            }
+            return null;
+        }
     }
+
 }
