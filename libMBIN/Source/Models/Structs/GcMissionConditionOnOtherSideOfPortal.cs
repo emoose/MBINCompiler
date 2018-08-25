467a958f8d3c1fd3b9b23864087b5bd83d2a89bd
HP <41843643+TheBob12@users.noreply.github.com>
2018-08-25 12:18:43 +0200

    Added a few structs and updated existing ones (#67)
    * Added a few structs and updated existing ones
    
    * Cleaned file up
    
    forgot to remove using lists, after copying an existing struct...

---
+++ libMBIN/Source/Models/Structs/GcMissionConditionOnOtherSideOfPortal.cs
@@ -0,0 +1,9 @@
+﻿namespace libMBIN.Models.Structs
+{
+    [NMS(Size = 0x2)]
+    public class GcMissionConditionOnOtherSideOfPortal : NMSTemplate
+    {
+        /* 0x0 */ public bool TestForRegularPortal;
+        /* 0x1 */ public bool TestForStoryPortal;
+    }
+}
---
+++ libMBIN/Source/Models/Structs/TkLODComponentData.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace libMBIN.Models.Structs
+{
+    [NMS(Size = 0x18)]
+    public class TkLODComponentData : NMSTemplate
+    {
+        /* 0x10 */ public List<TkLODModelResource> LODModel;
+        /* 0x10 */ public float CrossFadeTime;
+        /* 0x14 */ public float CrossFadeOverlap;
+    }
+}
