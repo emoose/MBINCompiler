1e477c30b6a04656af96bc76715b79630a48079a
Gaticus Hax <gaticus.hax@gmail.com>
2018-09-12 01:54:01 -0700

    fix for failing test in IntRevTests (floating-point inaccuracies)

--- MBINCompilerTests/Source/Tests/IntRevTest.cs
+++ MBINCompilerTests/Source/Tests/IntRevTest.cs
@@ -6,7 +6,7 @@ namespace MBINCompilerTests
 {
 
     [TestClass]
-    public class IntRevTest
+    public class IntRevTests
     {
 
         [TestMethod]
@@ -14,7 +14,19 @@ namespace MBINCompilerTests
         {
             float[] actualValue = INT_2_10_10_10_REV.FromBytes(1592787842);
             float[] expectedValue = new float[4] { -0.24667928063493072f, 0.0f, 0.9690971739229421f, 1.0f };
-            Assert.AreEqual(expectedValue, actualValue, $"Assert Failed: Expected [{expectedValue[0]}, {expectedValue[1]}, {expectedValue[2]}, {expectedValue[3]}] but got [{actualValue[0]}, {actualValue[1]}, {actualValue[2]}, {actualValue[3]}]");
+
+            // accommodate floating-point inaccuracies
+            bool pass = true;
+            pass &= Math.Abs( expectedValue[0] - actualValue[0] ) <= 1e-7f;
+            pass &= Math.Abs( expectedValue[1] - actualValue[1] ) <= 1e-7f;
+            pass &= Math.Abs( expectedValue[2] - actualValue[2] ) <= 1e-7f;
+            pass &= Math.Abs( expectedValue[3] - actualValue[3] ) <= 1e-7f;
+
+            Assert.IsTrue( pass,
+                $"Assert Failed: Expected " +
+                $"[{expectedValue[0]}, {expectedValue[1]}, {expectedValue[2]}, {expectedValue[3]}]" +
+                $" but got " +
+                $"[{actualValue[0]}, {actualValue[1]}, {actualValue[2]}, {actualValue[3]}]" );
         }
 
         [TestMethod]
@@ -22,7 +34,7 @@ namespace MBINCompilerTests
         {
             int actualValue = INT_2_10_10_10_REV.FromVerts(new float[4] { -0.24667928063493072f, 0.0f, 0.9690971739229421f, 1.0f });
             int expectedValue = 1592787842;
-            Assert.AreEqual(expectedValue, actualValue, $"Assert Failed: Expected [{expectedValue}] but got [{actualValue}]");
+            Assert.AreEqual( expectedValue, actualValue, $"Assert Failed: Expected [{expectedValue}] but got [{actualValue}]" );
         }
     }
 }
