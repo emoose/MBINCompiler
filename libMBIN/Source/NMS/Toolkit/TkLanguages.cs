02f09eff8df1fcf2cf1eb79d001624b707c945f3
Gaticus Hax <gaticus.hax@gmail.com>
2018-09-12 03:27:53 -0700

    fix missing enum value in TkLanguages - fixes METADATA\LANGUAGE\ALLOWEDLANGUAGE_AS.MBIN - fixes METADATA\FONTS\LANGUAGEFONTOVERRIDETABLE.MBIN

--- libMBIN/Source/NMS/Toolkit/TkLanguages.cs
+++ libMBIN/Source/NMS/Toolkit/TkLanguages.cs
@@ -6,7 +6,7 @@ namespace libMBIN.NMS.Toolkit
 	[NMS(GUID = 0x86B61CEEE4451AD)]
     public class TkLanguages : NMSTemplate
     {
-		public enum LanguageEnum { Default, English, USEnglish, French, Italian, German, Spanish, Russian, Polish, Dutch, Portuguese, LatinAmericanSpanish, BrazilianPortuguese, Japanese, TraditionalChinese, SimplifiedChinese, Korean }
+		public enum LanguageEnum { Default, English, USEnglish, French, Italian, German, Spanish, Russian, Polish, Dutch, Portuguese, LatinAmericanSpanish, BrazilianPortuguese, Japanese, TraditionalChinese, SimplifiedChinese, TencentChinese, Korean }
 		public LanguageEnum Language;
     }
 }
