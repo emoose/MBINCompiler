1166f551fae4ae4d85a7bd60880cd9ebbdc5e7df
Gaticus Hax <gaticus.hax@gmail.com>
2018-11-28 04:22:39 -0800

    Couple of updates. - Added TkFeaturesEnum - Updated GcPlanetGenerationInputData

--- libMBIN/Source/NMS/GameComponents/GcPlanetGenerationInputData.cs
+++ libMBIN/Source/NMS/GameComponents/GcPlanetGenerationInputData.cs
@@ -3,20 +3,22 @@ using libMBIN.NMS.GameComponents;
 
 namespace libMBIN.NMS.GameComponents
 {
-	[NMS(Size = 0x48, GUID = 0xCA4B078251BAF8BE)]
+	[NMS(Size = 0x48, GUID = 0xDE8BF371F6565035)]
     public class GcPlanetGenerationInputData : NMSTemplate
     {
         /* 0x00 */ public GcSeed Seed;
-        /* 0x10 */ public GcGalaxyStarTypes StarType;
-        /* 0x14 */ public GcPlanetClass PlanetClass;
+        /* 0x10 */ public GcGalaxyStarTypes Star;
+        /* 0x14 */ public GcPlanetClass Class;
         [NMS(Size = 0x10)]
         /* 0x18 */ public string CommonSubstance;
         [NMS(Size = 0x10)]
         /* 0x28 */ public string RareSubstance;
         /* 0x38 */ public GcPlanetSize PlanetSize;
         /* 0x3C */ public GcBiomeType Biome;
-        /* 0x40 */ public int BiomeFileIndex;
-        [NMS(Size = 0x4, Ignore = true)]
+        /* 0x40 */ public GcBiomeSubType BiomeSubType;
+        /* 0x45 */ public bool HasRings;
+        /* 0x46 */ public bool ForceContinents;
+        [NMS(Size = 0x2, Ignore = true)]
         public byte[] EndPadding;
     }
 }
---
+++ libMBIN/Source/NMS/Toolkit/TkFeaturesEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace libMBIN.NMS.Toolkit {
+
+    [NMS( GUID = 0xFAFFDE9C9C0261C1 )]
+    public class TkFeaturesEnum : NMSTemplate {
+
+        public enum FeatureTypesEnum { River, Crater, Arches, ArchesSmall, Blobs, BlobsSmall, Substance }
+        public int FeatureTypes;
+
+    }
+
+}
