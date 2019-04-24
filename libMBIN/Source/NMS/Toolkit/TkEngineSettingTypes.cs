b4c48aa5e91d830b51b7d8ffbfea3ced4654c34c
monkeyman192 <monkey_man_192@yahoo.com.au>
2019-04-24 22:44:40 +1000

    updated graphics settings structs

---
+++ libMBIN/Source/NMS/Toolkit/TkEngineSettingTypes.cs
@@ -0,0 +1,15 @@
+﻿namespace libMBIN.NMS.Toolkit {
+
+    [NMS( GUID = 0xF859EBED47A81E0A)]
+    public class TkEngineSettingTypes : NMSTemplate {
+
+        public enum EngineSettingEnum { FullScreen, Borderless, ResolutionWidth, ResolutionHeight, Monitor, FoVOnFoot, FoVInShip, VSync, GSync, ShadowDetail,
+                                        TextureDetail, GenerationDetail, ReflectionsQuality, AntiAliasing, MotionBlurQuality, AnisotropyLevel, Brightness,
+                                        AvailableMonitors, FrameRate, NumLowThreads, NumHighThreads, NumGraphicsThreads, TextureStreaming, TexturePageSizeKb,
+                                        MotionBlurStrength, ShowRequirementsWarnings, AmbientOcclusion, UseLightshafts, MaxTextureMemoryMb, FixedTextureMemory,
+                                        EnableTessellation, UseArbSparseTexture, TerrainQuality, UseTerrainTextureCache, AdapterIndex, UseHDR }
+        public int EngineSetting;
+
+    }
+
+}
--- libMBIN/Source/NMS/Toolkit/TkGraphicsSettings.cs
+++ libMBIN/Source/NMS/Toolkit/TkGraphicsSettings.cs
@@ -5,7 +5,7 @@ using libMBIN.NMS.GameComponents;
 
 namespace libMBIN.NMS.Toolkit
 {
-	[NMS(Size = 0x88, GUID = 0xD6DA8D9BCBAC1DBB)]
+	[NMS(Size = 0x198, GUID = 0xB1EE0DFF77318199)]
     public class TkGraphicsSettings : NMSTemplate
     {
         /* 0x00 */ public int Version;
@@ -18,12 +18,12 @@ namespace libMBIN.NMS.Toolkit
 		/* 0x20 */ public bool UseScreenResolution;
         /* 0x24 */ public int ResolutionWidth;
         /* 0x28 */ public int ResolutionHeight;
-		public enum VsyncExEnum { Off, On, Adaptive }
+		public enum VsyncExEnum { Off, On, Adaptive, Triple }
         /* 0x2C */ public VsyncExEnum VsyncEx;
         /* 0x30 */ public bool GSync;
 		public enum ShadowDetailEnum { Low, Normal, High, Ultra }
 		/* 0x34 */ public ShadowDetailEnum ShadowDetail;
-		public enum TextureDetailEnum { Low, Normal, High }
+		public enum TextureDetailEnum { Low, Normal, High, Ultra }
 		/* 0x38 */ public TextureDetailEnum TextureDetail;
 		public enum GenerationDetailEnum { Low, Normal, High }
 		/* 0x3C */ public GenerationDetailEnum GenerationDetail;
@@ -43,8 +43,8 @@ namespace libMBIN.NMS.Toolkit
         /* 0x64 */ public int MaxframeRate;
 		/* 0x68 */ public int NumHighThreads;
 		/* 0x6C */ public int NumLowThreads;
-		public enum TextureStreamingExEnum { Off, On, Auto, NonDynamic }
-		/* 0x70 */ public TextureStreamingExEnum TextureStreamingEx;
+		public enum TextureStreamingVkEnum { Off, On, Auto, NonDynamic }
+		/* 0x70 */ public TextureStreamingVkEnum TextureStreamingVk;
 		/* 0x74 */ public bool ShowRequirementsWarnings;
 		/* 0x75 */ public bool RemoveBaseBuildingRestrictions;
         /* 0x78 */ public float MouseClickSpeedMultiplier;
@@ -53,9 +53,16 @@ namespace libMBIN.NMS.Toolkit
         /* 0x80 */ public bool UseTerrainTextureCache;
         /* 0x81 */ public bool UseArbSparseTexture;
         /* 0x82 */ public bool EnableTessellation;
-        /* 0x83 */ public bool UseHDR;
-        /* 0x84 */ public bool UseLightShafts;
+        public enum HDRModeEnum { Off, HDR400, HDR600, HDR1000 }
+        /* 0x84 */ public HDRModeEnum HDRMode;
+        /* 0x88 */ public bool UseLightShafts;
+        [NMS(Size = 0x100)]
+        /* 0x89 */ public string AdapterName;
         [NMS(Size = 0x3, Ignore = true)]
-        /* 0x85 */ public byte[] EndPadding;
+        /* 0x18A */ public byte[] Padding18A;
+        /* 0x18C */ public int AdapterIndex;
+        /* 0x190 */ public int NumGraphicsThreadsBeta;
+        [NMS(Size = 0x4, Ignore = true)]
+        /* 0x194 */ public byte[] EndPadding;
     }
 }
