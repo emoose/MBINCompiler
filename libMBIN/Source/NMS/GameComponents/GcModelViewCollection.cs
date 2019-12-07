0e16db603b454fb294e41f378c573e58936dfba0
HolterPhylo <robert.leduc@gmail.com>
2019-12-07 08:43:25 -0500

    Updated GcModelViewCollection (#186)
    * - Updated GcModelViewCollection

--- libMBIN/Source/NMS/GameComponents/GcModelViewCollection.cs
+++ libMBIN/Source/NMS/GameComponents/GcModelViewCollection.cs
@@ -3,15 +3,15 @@ using libMBIN.NMS.GameComponents;
 
 namespace libMBIN.NMS.GameComponents
 {
-	[NMS(Size = 0x1080, GUID = 0x3B4CC3DEE46D547D, NameHash = 0xC3362B34D26192B6)]
+	[NMS(Size = 0x1130, GUID = 0xED972786E8177E52, NameHash = 0xC3362B34D26192B6)]
     public class GcModelViewCollection : NMSTemplate
     {
         public enum ModelViewEnum {
-            Inactive, Suit, Weapon, Ship, Vehicle, DiscoveryMain, DiscoveryThumbnail, ToolboxMain, ToolboxThumbnail, TradeSuit,
+            Suit, Weapon, Ship, Vehicle, DiscoveryMain, DiscoveryThumbnail, ToolboxMain, ToolboxThumbnail, TradeSuit,
             TradeShip, TradeCompareShips, TradeCompareWeapons, HUDThumbnail, Interaction, Freighter, TradeFreighter, TradeChest, TradeCapsule, TradeFrigate,
-            TerrainBall, FreighterChest, Submarine, TradeCooker
+            TerrainBall, FreighterChest, Submarine, TradeCooker, SuitRefiner, FreighterRepair
         }
-        [NMS(Size = 0x18, EnumType = typeof(ModelViewEnum))]
+        [NMS(Size = 0x19, EnumType = typeof(ModelViewEnum))]
         public TkModelRendererData[] ModelViews;
     }
 }
