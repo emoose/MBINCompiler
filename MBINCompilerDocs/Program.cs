38bb65f6a716f07631575eb15a704dc5345129bf
monkeyman192 <monkey_man_192@yahoo.com.au>
2020-09-20 12:35:06 +1000

    change struct -> class and start adding script in travis config

--- MBINCompilerDocs/Program.cs
+++ MBINCompilerDocs/Program.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 using libMBIN;
 using libMBIN.NMS.GameComponents;
 using libMBIN.NMS.Toolkit;
-//using libMBIN.Models.Structs;
+
 
 namespace MBINCompilerDocs
 {
@@ -21,7 +21,7 @@ namespace MBINCompilerDocs
             { "Single", "Float" },
         };
 
-        static string basePath = Path.Combine("..", "..", "docs", "structs");
+        static string basePath = Path.Combine("..", "..", "docs", "classes");
 
         //Create a mapping between the full names of tk and gc and the short-hands
         internal static readonly Dictionary<string, string> NMS_dir_mapping = new Dictionary<string, string>
@@ -45,14 +45,14 @@ namespace MBINCompilerDocs
             }
         }
 
-        static string GetNMSType(Type structType)
+        static string GetNMSType(Type classType)
         {
-            if (structType == typeof(NMSTemplate)) {
+            if (classType == typeof(NMSTemplate)) {
                 // We hard-code this since it's namespace won't map to anything.
                 // TODO: Maybe have 'libmbin' map to nms? Might be risky though...
                 return "nms";
             }
-            var splitname = structType.Namespace.Split('.');
+            var splitname = classType.Namespace.Split('.');
             string last = splitname[splitname.Length - 1];
             if (NMS_dir_mapping.ContainsKey(last)) {
                 return NMS_dir_mapping[last];
@@ -82,8 +82,8 @@ namespace MBINCompilerDocs
                 int? size = t.GetCustomAttribute<NMSAttribute>()?.Size;
                 ulong? guid = t.GetCustomAttribute<NMSAttribute>()?.GUID;
 
-                // Write the struct name and GUID
-                md.AppendLine($"## [All structs](../../) > [{NMSType}](../) > {name}");
+                // Write the class name and GUID
+                md.AppendLine($"## [All classes](../../) > [{NMSType}](../) > {name}");
                 if (t.Namespace != "libMBIN.NMS") {
                     md.AppendLine($"**Size**: 0x{size:X}, **GUID**: 0x{guid:X}\n");
                 }
@@ -105,18 +105,18 @@ namespace MBINCompilerDocs
                 foreach (var field in fields) {
                     var field_attrs = field.GetCustomAttribute<NMSAttribute>();
 
-                    // For each field in the struct, write a row in the table
+                    // For each field in the class, write a row in the table
                     if (!(field.GetCustomAttribute<NMSAttribute>()?.Ignore ?? false)) {
                         var fieldType = field.FieldType;
                         // Write the field name first
                         md.Append($"| {field.Name} |");
 
                         // If the base type is based on NMSTemplate, then we want to create a hyperlink to the page.
-                        string structName = $"{ConvertDatatype(fieldType)}";
+                        string className = $"{ConvertDatatype(fieldType)}";
                         if (fieldType.BaseType == typeof(NMSTemplate) || fieldType == typeof(NMSTemplate)) {
                             var fieldNMSType = GetNMSType(fieldType);
                             if (fieldNMSType != null) {
-                                structName = $"[{fieldType.Name}](../../../structs/{fieldNMSType}/{fieldType.Name})";
+                                className = $"[{fieldType.Name}](../../../classes/{fieldNMSType}/{fieldType.Name})";
                             }
                         }
 
@@ -142,35 +142,35 @@ namespace MBINCompilerDocs
                         else if (fieldType.IsArray) {
                             int? arrSize = field.GetCustomAttribute<NMSAttribute>()?.Size;
                             var arrayEnumType = field.GetCustomAttribute<NMSAttribute>()?.EnumType;
-                            structName = structName.Substring(0, structName.Length - 2);
+                            className = className.Substring(0, className.Length - 2);
                             // For enums, get the values it can have:
                             if (arrayEnumType != null && arrayEnumType.IsEnum) {
-                                md.Append($"<div class='expandable'>{structName}[0x{arrSize:X}]</div><ul class='enumValues' hidden>");
+                                md.Append($"<div class='expandable'>{className}[0x{arrSize:X}]</div><ul class='enumValues' hidden>");
                                 foreach (var enumValue in Enum.GetValues(arrayEnumType)) {
                                     md.Append($"<li>{enumValue.ToString()}</li>");
                                 }
                                 md.Append("</ul>");
                             } else {
-                                md.Append($"{structName}[0x{arrSize:X}]");
+                                md.Append($"{className}[0x{arrSize:X}]");
                             }
                         }
                         // For a list, get the list type:
                         else if (fieldType.IsGenericType && fieldType.GetGenericTypeDefinition() == typeof(List<>)) {
                             var refType = field.FieldType.GetGenericArguments()[0];
                             string dtypeName = ConvertDatatype(refType);
-                            string listStructName = $"{dtypeName}";
+                            string listClassName = $"{dtypeName}";
                             if (refType.BaseType == typeof(NMSTemplate) || refType == typeof(NMSTemplate)) {
                                 var listNMSType = GetNMSType(refType);
                                 if (listNMSType != null) {
-                                    listStructName = $"[{dtypeName}](../../../structs/{listNMSType}/{dtypeName})";
+                                    listClassName = $"[{dtypeName}](../../../classes/{listNMSType}/{dtypeName})";
                                 }
                             }
-                            md.Append($"List<{listStructName}\\>");
+                            md.Append($"List<{listClassName}\\>");
                         }
                         // This is the case of a NMSTemplate or standard type.
                         // TODO: handle the NMSTemplate case here and above...
                         else {
-                            md.Append($"{structName}");
+                            md.Append($"{className}");
                         }
                         // See if the field has a description
                         string desc = field.GetCustomAttribute<NMSDescription>()?.Description;
@@ -197,11 +197,11 @@ namespace MBINCompilerDocs
                 int? size = t.GetCustomAttribute<NMSAttribute>()?.Size;
 
                 // Write the name and size.
-                XmlElement structNode = xmlDoc.CreateElement(name);
+                XmlElement classNode = xmlDoc.CreateElement(name);
 
                 XmlAttribute attribute = xmlDoc.CreateAttribute("size");
                 attribute.Value = $"0x{size:X}";
-                structNode.Attributes.Append(attribute);
+                classNode.Attributes.Append(attribute);
 
                 // Get fields in order of declaration
                 var fields = t.GetFields().OrderBy(field => field.MetadataToken);
@@ -248,18 +248,18 @@ namespace MBINCompilerDocs
 
                         typeAttr.Value = fieldType.Name;
                         fieldNode.Attributes.Append(typeAttr);
-                        structNode.AppendChild(fieldNode);
+                        classNode.AppendChild(fieldNode);
                     }
                 }
-                tkNode.AppendChild(structNode);
+                tkNode.AppendChild(classNode);
                 xmlDoc.AppendChild(tkNode);
 
-                xmlDoc.Save($"{outName}_structs.xml");
+                xmlDoc.Save($"{outName}_classes.xml");
                 Console.WriteLine($"template: {name}, size: 0x{size:X}");
             }
         }
 
-        static void update_struct_list(List<Type> classes, StringBuilder md, bool includeDir = false) {
+        static void update_class_list(List<Type> classes, StringBuilder md, bool includeDir = false) {
             foreach (var t in classes) {
                 //Console.WriteLine(t.Name);
                 var classNMSType = GetNMSType(t);
@@ -310,9 +310,9 @@ namespace MBINCompilerDocs
             // Create the directory if needed
             GenerateDirs();
 
-            // Write all the structs to a single file
+            // Write all the classes to a single file
             StringBuilder md = new StringBuilder();
-            md.AppendLine("# List of all structs");
+            md.AppendLine("# List of all Classes");
             md.AppendLine();
             md.AppendLine("### [Base classes](nms)");
             md.AppendLine();
@@ -321,67 +321,79 @@ namespace MBINCompilerDocs
             md.AppendLine();
             md.AppendLine("| Name | Size |");
             md.AppendLine("| ---- | ---- |");
-            update_struct_list(base_classes, md, true);
+            md.AppendLine("| [NMSTemplate](nms/NMSTemplate) | 0x48 |");
+            update_class_list(base_classes, md, true);
             md.AppendLine();
             md.AppendLine("### [Globals](globals)");
             md.AppendLine();
-            md.AppendLine("Globals are structs which are a fixed size and vary many aspects of the game (hence the name!).");
+            md.AppendLine("Globals are classes which are a fixed size and vary many aspects of the game (hence the name!).");
             md.AppendLine();
             md.AppendLine("| Name | GUID | Size |");
             md.AppendLine("| ---- | ---- | ---- |");
-            update_struct_list(globals_classes, md, true);
+            update_class_list(globals_classes, md, true);
             md.AppendLine();
             md.AppendLine("### [GameComponents (Gc)](gc)");
             md.AppendLine();
             md.AppendLine("| Name | GUID | Size |");
             md.AppendLine("| ---- | ---- | ---- |");
-            update_struct_list(gc_classes, md, true);
+            update_class_list(gc_classes, md, true);
             md.AppendLine();
             md.AppendLine("### [Toolkits (Tk)](tk)");
             md.AppendLine();
             md.AppendLine("| Name | GUID | Size |");
             md.AppendLine("| ---- | ---- | ---- |");
-            update_struct_list(tk_classes, md, true);
+            update_class_list(tk_classes, md, true);
             File.WriteAllText(Path.Combine(basePath, "index.md"), md.ToString());
 
             // Also write each index of classes to its own file
             StringBuilder md_base = new StringBuilder();
-            md_base.AppendLine("# [All structs](../) > Base classes");
+            md_base.AppendLine("# [All classes](../) > Base classes");
             md_base.AppendLine();
-            md_base.AppendLine("| Name |");
-            md_base.AppendLine("| ---- |");
-            update_struct_list(base_classes, md_base);
+            md_base.AppendLine("| Name | Size |");
+            md_base.AppendLine("| ---- | ---- |");
+            md_base.AppendLine("| [NMSTemplate](NMSTemplate) | 0x48 |");
+            update_class_list(base_classes, md_base);
             File.WriteAllText(Path.Combine(basePath, "nms", "index.md"), md_base.ToString());
 
             StringBuilder md_gc = new StringBuilder();
-            md_gc.AppendLine("# [All structs](../) > GameComponent (Gc)");
+            md_gc.AppendLine("# [All classes](../) > GameComponent (Gc)");
             md_gc.AppendLine();
             md_gc.AppendLine("| Name | GUID | Size |");
             md_gc.AppendLine("| ---- | ---- | ---- |");
-            update_struct_list(gc_classes, md_gc);
+            update_class_list(gc_classes, md_gc);
             File.WriteAllText(Path.Combine(basePath, "gc", "index.md"), md_gc.ToString());
 
             StringBuilder md_tk = new StringBuilder();
-            md_tk.AppendLine("# [All structs](../) > Tookit (Tk)");
+            md_tk.AppendLine("# [All classes](../) > Tookit (Tk)");
             md_tk.AppendLine();
             md_tk.AppendLine("| Name | GUID | Size |");
             md_tk.AppendLine("| ---- | ---- | ---- |");
-            update_struct_list(tk_classes, md_tk);
+            update_class_list(tk_classes, md_tk);
             File.WriteAllText(Path.Combine(basePath, "tk", "index.md"), md_tk.ToString());
 
             StringBuilder md_globals = new StringBuilder();
-            md_globals.AppendLine("# [All structs](../) > Globals");
+            md_globals.AppendLine("# [All classes](../) > Globals");
             md_globals.AppendLine();
             md_globals.AppendLine("| Name | GUID | Size |");
             md_globals.AppendLine("| ---- | ---- | ---- |");
-            update_struct_list(globals_classes, md_globals);
+            update_class_list(globals_classes, md_globals);
             File.WriteAllText(Path.Combine(basePath, "globals", "index.md"), md_globals.ToString());
             export_md(base_classes);
             export_md(tk_classes);
             export_md(gc_classes);
             export_md(globals_classes);
-            //export_xml(tk_classes, "Tk");
-            WaitForKeypress();
+
+            // Write the NMSTemplate info
+            StringBuilder md_NMSTemplate = new StringBuilder();
+            md_NMSTemplate.AppendLine("## [All classes](../../) > [nms](../) > NMSTemplate");
+            md_NMSTemplate.AppendLine();
+            md_NMSTemplate.AppendLine("This is the base class that all others are subclassed from.");
+            md_NMSTemplate.Append("This class also has a number of methods available to it (and be extension, all subclasses) ");
+            md_NMSTemplate.Append("which can be found [here](../../../libmbin/usage/#nmstemplate)");
+            md_NMSTemplate.AppendLine();
+            File.WriteAllText(Path.Combine(basePath, "nms", "NMSTemplate.md"), md_NMSTemplate.ToString());
+
+            //WaitForKeypress();
         }
     }
 }
