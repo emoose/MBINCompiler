e5d732e68073ba48db7ab3c4e79f2008170e2e43
HolterPhylo <robertled@gmail.com>
2021-01-14 18:33:46 -0500

    Corrected enum

--- libMBIN/Source/NMS/GameComponents/GcVehicleType.cs
+++ libMBIN/Source/NMS/GameComponents/GcVehicleType.cs
@@ -6,7 +6,7 @@ namespace libMBIN.NMS.GameComponents
 	[NMS(Size = 0x4, GUID = 0xD3947F166802A50B, NameHash = 0x46812D05ED43A48D)]
     public class GcVehicleType : NMSTemplate
     {
-		public enum VehicleTypeEnum { Bike, Buggy, Truck, WheeledBike, Hovercraft, Submarine, Mech }
+		public enum VehicleTypeEnum { Buggy, Bike, Truck, WheeledBike, Hovercraft, Submarine, Mech }
 		public VehicleTypeEnum VehicleType;
     }
 }
