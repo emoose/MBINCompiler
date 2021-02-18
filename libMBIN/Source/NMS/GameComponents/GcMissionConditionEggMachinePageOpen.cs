910cd8bcde92cd6e9fb79c9237e7bf8d7a926f0a
Wbertro <robert.leduc@gmail.com>
2021-02-18 17:05:33 -0500

    Update 6254225 (#299)
    * Update 6254225: Templates and libMBIN-Shared.projitems update
    
    Co-authored-by: HolterPhylo <robertled@gmail.com>

---
+++ libMBIN/Source/NMS/GameComponents/GcMissionConditionEggMachinePageOpen.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+using libMBIN.NMS.Toolkit;
+using libMBIN.NMS.GameComponents;
+
+namespace libMBIN.NMS.GameComponents
+{
+    [NMS(Size = 0x1, GUID = 0x0847A0F162F2B1F3, NameHash = 0xE1F519AEBA39D7A0, Broken = true)]
+    public class GcMissionConditionEggMachinePageOpen : NMSTemplate
+    {
+    }
+}
