d400a0e105df4977ff824031866ddb7f94aa23fa
HolterPhylo <robertled@gmail.com>
2021-06-11 16:31:45 -0400

    Update GcSmokeBotReport struct

--- libMBIN/Source/NMS/GameComponents/GcSmokeBotReport.cs
+++ libMBIN/Source/NMS/GameComponents/GcSmokeBotReport.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 using libMBIN.NMS.Toolkit;
@@ -8,7 +9,7 @@ namespace libMBIN.NMS.GameComponents
     [NMS(Size = 0x18, GUID = 0x3BC2543792E9B519, NameHash = 0x16AC643981D10CC9, Broken = true)]
     public class GcSmokeBotReport : NMSTemplate
     {
-        /* 0x00 */ ulong StartingUA;
-        /* 0x08 */ List<GcSmokeBotSystemReport> Systems;
+        /* 0x00 */ public ulong StartingUA;
+        /* 0x08 */ public List<GcSmokeBotSystemReport> Systems;
     }
 }
