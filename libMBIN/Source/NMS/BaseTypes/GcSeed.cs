505985da41368c5a31048e7e90e5acddbd7b4ce8
Chris Purnell <cjp@lost.org.uk>
2021-06-12 14:09:22 +0100

    Remove unneeded end padding fields from base types.

--- libMBIN/Source/NMS/BaseTypes/GcSeed.cs
+++ libMBIN/Source/NMS/BaseTypes/GcSeed.cs
@@ -5,8 +5,6 @@
     {
         /* 0x0 */ public long Seed;
         /* 0x8 */ public bool UseSeedValue;
-        [NMS(Size = 7, Ignore = true)]
-        /* 0x9 */ public byte[] EndPadding;
     }
 
 }
