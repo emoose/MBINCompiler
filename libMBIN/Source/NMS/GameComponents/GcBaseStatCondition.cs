c512593a98bed22c5aa7de87fccf1ad5481b9328
HolterPhylo <robertled@gmail.com>
2021-09-04 10:55:51 -0400

    New structures for 7302483

---
+++ libMBIN/Source/NMS/GameComponents/GcBaseStatCondition.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+using libMBIN.NMS.Toolkit;
+using libMBIN.NMS.GameComponents;
+
+namespace libMBIN.NMS.GameComponents
+{
+    [NMS(Size = 0x8, GUID = 0x3A55F7D75764533A, NameHash = 0xB05E7D6A559B1AC0, Broken = true)]
+    public class GcBaseStatCondition : NMSTemplate
+    {
+    }
+}
---
+++ libMBIN/Source/NMS/GameComponents/GcBuildingModeCondition.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+using libMBIN.NMS.Toolkit;
+using libMBIN.NMS.GameComponents;
+
+namespace libMBIN.NMS.GameComponents
+{
+    [NMS(Size = 0x10, GUID = 0x9BA8DB2D80F3B645, NameHash = 0x307D1ADF8E3AA83B, Broken = true)]
+    public class GcBuildingModeCondition : NMSTemplate
+    {
+    }
+}
