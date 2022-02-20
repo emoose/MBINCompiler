8cc9158cf4b340d540c92057029d99d0e1bd4d5c
HolterPhylo <robertled@gmail.com>
2022-02-19 23:52:41 -0500

    Corrected RobotGlobals For 8223629

--- libMBIN/Source/NMS/Globals/GcRobotGlobals.cs
+++ libMBIN/Source/NMS/Globals/GcRobotGlobals.cs
@@ -114,6 +114,8 @@ namespace libMBIN.NMS.Globals
         /* 0x0208 */ public float DroneInvestigateMinScanTime;
         /* 0x020C */ public float MedicDroneMinHealTime;
         /* 0x0210 */ public GcScanEffectData DroneScanEffect;
+        [NMS(Size = 0x9, EnumType = typeof(/*>>>>>>???<<<<<<*/))]
+        /* 0x0260 */ public int[] SentinelSpawnLimits;
         /* 0x0794 */ public float HeightTestSampleDistance;
         /* 0x0798 */ public float HeightTestSampleTime;
         /* 0x079C */ public float CollisionDistance;
