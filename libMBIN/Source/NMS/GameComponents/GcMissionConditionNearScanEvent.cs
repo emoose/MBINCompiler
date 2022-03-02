70e9c8395730ab4621d64df792fd7a4f7faa1a04
monkeyman192 <monkey_man_192@yahoo.com.au>
2022-03-02 23:34:04 +1100

    Few small fixes for latest experimental

--- libMBIN/Source/NMS/GameComponents/GcMissionConditionNearScanEvent.cs
+++ libMBIN/Source/NMS/GameComponents/GcMissionConditionNearScanEvent.cs
@@ -5,12 +5,13 @@ using libMBIN.NMS.GameComponents;
 
 namespace libMBIN.NMS.GameComponents
 {
-	[NMS(Size = 0x28, GUID = 0xE31955C81647FC37, NameHash = 0x8F7E57B730A9C7F8)]
+	[NMS(Size = 0x28, GUID = 0x4E25FE0EFD798303, NameHash = 0x8F7E57B730A9C7F8)]
     public class GcMissionConditionNearScanEvent : NMSTemplate
     {
         /* 0x00 */ public NMSString0x20A Event;
         /* 0x20 */ public float Distance;
         /* 0x24 */ public bool AllowInShip;
         /* 0x25 */ public bool RequiresFullFireteam;
+        /* 0x26 */ public bool ReturnTrueIfMarkerGone;
     }
 }
--- libMBIN/Source/NMS/GameComponents/GcMissionSequenceCompleteSettlementJudgement.cs
+++ libMBIN/Source/NMS/GameComponents/GcMissionSequenceCompleteSettlementJudgement.cs
@@ -3,11 +3,12 @@ using libMBIN.NMS.GameComponents;
 
 namespace libMBIN.NMS.GameComponents
 {
-    [NMS(Size = 0xB00, GUID = 0xBE7FDC03159FE877, NameHash = 0x35FB9E28116A4F2A)]
+    [NMS(Size = 0xC80, GUID = 0xA42786487036AD06, NameHash = 0x35FB9E28116A4F2A)]
     public class GcMissionSequenceCompleteSettlementJudgement : NMSTemplate
     {
         [NMS(Size = 0x7, EnumType = typeof(GcSettlementJudgementType.SettlementJudgementTypeEnum))]
         /* 0x000 */ public GcJudgementMessageOptions[] MessageOptions;
-        /* 0xA80 */ public NMSString0x80 DebugText;
+        /* 0xA80 */ public GcJudgementMessageOptions MessageNoOffice;
+        /* 0xC00 */ public NMSString0x80 DebugText;
     }
 }
