edd7950f87bf4114e478b255d1102bb4ae3731ed
HolterPhylo <robertled@gmail.com>
2022-04-22 17:39:00 -0400

    Corrected GUID

--- libMBIN/Source/NMS/Toolkit/TkProceduralTextureList.cs
+++ libMBIN/Source/NMS/Toolkit/TkProceduralTextureList.cs
@@ -6,7 +6,7 @@ using libMBIN.NMS.GameComponents;
 
 namespace libMBIN.NMS.Toolkit
 {
-    [NMS(Size = 0x200, GUID = 0x2C04CD0BD6534FA0, NameHash = 0xBEE79269D7359654)]
+    [NMS(Size = 0x200, GUID = 0xECEAC32925156DF1, NameHash = 0xBEE79269D7359654)]
     public class TkProceduralTextureList : NMSTemplate
     {
         [NMS(Size = 0x8)]
