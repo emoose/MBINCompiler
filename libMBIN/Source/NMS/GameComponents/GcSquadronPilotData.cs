18b2093c4956e857cd365133b8aeea226f2ea378
HolterPhylo <robertled@gmail.com>
2022-04-27 12:15:28 -0400

    Update for 8628165

--- libMBIN/Source/NMS/GameComponents/GcSquadronPilotData.cs
+++ libMBIN/Source/NMS/GameComponents/GcSquadronPilotData.cs
@@ -6,11 +6,12 @@ using libMBIN.NMS.GameComponents;
 
 namespace libMBIN.NMS.GameComponents
 {
-    [NMS(Size = 0x558, GUID = 0x68E27A3C3EF27F5F, NameHash = 0x1B17AE774F200920)]
+    [NMS(Size = 0x560, GUID = 0xEED778FAACCB1F7D, NameHash = 0x1B17AE774F200920)]
     public class GcSquadronPilotData : NMSTemplate
     {
         /* 0x000 */ public GcResourceElement NPCResource;
         /* 0x2A8 */ public GcResourceElement ShipResource;
-        /* 0x550 */ public ushort PilotRank;
+        /* 0x550 */ public ulong TraitsSeed;
+        /* 0x558 */ public ushort PilotRank;
     }
 }
