9c00bb58949ab128b016c96b26f333aabe0e0e70
Thomas Margraff <thomaspmar@protonmail.com>
2022-06-25 21:32:32 -0700

    Added XML Doc Comment for NMSString0x100.ToString()

--- libMBIN/Source/NMS/BaseTypes/NMSString0x100.cs
+++ libMBIN/Source/NMS/BaseTypes/NMSString0x100.cs
@@ -14,6 +14,10 @@ namespace libMBIN.NMS
             return this.Value;
         }
 
+        /// <summary>
+        /// Returns the value held by this string.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             return Value;
