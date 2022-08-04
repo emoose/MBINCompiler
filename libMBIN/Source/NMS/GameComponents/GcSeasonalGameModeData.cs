e4e321f6045e3ca4a1db0c27c160a138418f3b37
HolterPhylo <robertled@gmail.com>
2022-08-03 21:13:07 -0400

    Update for 9246552-91349

--- libMBIN/Source/NMS/GameComponents/GcSeasonalGameModeData.cs
+++ libMBIN/Source/NMS/GameComponents/GcSeasonalGameModeData.cs
@@ -6,7 +6,7 @@ using libMBIN.NMS.GameComponents;
 
 namespace libMBIN.NMS.GameComponents
 {
-    [NMS(Size = 0x3370, GUID = 0x4F2D32DDF11EEAE, NameHash = 0xFD6B0BE30E218605)]
+    [NMS(Size = 0x3370, GUID = 0x7FA157ED8E92FC85, NameHash = 0xFD6B0BE30E218605)]
     public class GcSeasonalGameModeData : NMSTemplate
     {
         /* 0x0000 */ public int SeasonId;
