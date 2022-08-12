1c7d8cf83cc456fd5d08c96c6a41179376e600c5
HolterPhylo <robertled@gmail.com>
2022-08-11 21:32:13 -0400

    Reverted GcUIGlobals changes

--- libMBIN/Source/NMS/GameComponents/GcCreaturePetTraitRanges.cs
+++ libMBIN/Source/NMS/GameComponents/GcCreaturePetTraitRanges.cs
@@ -1,11 +1,11 @@
 using libMBIN.NMS.GameComponents;
-using System.Collections.Generic;
 
 namespace libMBIN.NMS.GameComponents
 {
-    [NMS(Size = 0x10, GUID = 0xE8D57E7EBF9DE772, NameHash = 0x55F9EF59F3B21D8)]
+    [NMS(Size = 0x18, GUID = 0xC0C793FFA12CE763, NameHash = 0x55F9EF59F3B21D8)]
     public class GcCreaturePetTraitRanges : NMSTemplate
     {
-        /* 0x0 */ public List<GcCreaturePetTraitRange> TraitRanges;
+        [NMS(Size = 0x3)]
+        /* 0x0 */ public GcCreaturePetTraitRange[] TraitRanges;
     }
 }
