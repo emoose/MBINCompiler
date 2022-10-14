2d114e6b60d753f1b2f48e3e1077d5c262b2cf7e
HolterPhylo <robertled@gmail.com>
2022-10-13 20:57:15 -0400

    Update for 9717058

--- libMBIN/Source/NMS/GameComponents/GcMissionConditionStatLevel.cs
+++ libMBIN/Source/NMS/GameComponents/GcMissionConditionStatLevel.cs
@@ -2,16 +2,17 @@ using libMBIN.NMS.Toolkit;
 
 namespace libMBIN.NMS.GameComponents
 {
-    [NMS(GUID = 0x9C42F3A1D0B5AE7D, NameHash = 0xA56864A865FEBE6D)]
+    [NMS(GUID = 0x69DE5108CB2EA1A2, NameHash = 0xA56864A865FEBE6D)]
     public class GcMissionConditionStatLevel : NMSTemplate
     {
         /* 0x00 */ public NMSString0x10 Stat;
         /* 0x10 */ public NMSString0x10 StatGroup;
         /* 0x20 */ public int Level;
-        /* 0x24 */ public bool TakeLevelFromSeasonData;
-        /* 0x25 */ public bool ForceIgnoreLevelledStat;
-        /* 0x28 */ public NMSString0x10 CompareStat;
-        /* 0x38 */ public int DisplayMilestoneNumber;
-        /* 0x3C */ public TkEqualityEnum Test;
+        /* 0x24 */ public int LevelledStatRank;
+        /* 0x28 */ public bool TakeLevelFromSeasonData;
+        /* 0x29 */ public bool ForceIgnoreLevelledStat;
+        /* 0x30 */ public NMSString0x10 CompareStat;
+        /* 0x40 */ public int DisplayMilestoneNumber;
+        /* 0x44 */ public TkEqualityEnum Test;
     }
 }
