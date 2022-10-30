1b4a39f66f84e6e4581a5c44d0ac59313e724a76
monkeyman192 <monkey_man_192@yahoo.com.au>
2022-10-30 18:01:29 +1100

    Batch convert test mbin files to speed up tests

--- tests/bytecompare_test.py
+++ tests/bytecompare_test.py
@@ -2,11 +2,10 @@ import pytest
 import os.path as op
 import os
 
-from utils import compare_mbins, convert_mbin, failed_files
+from utils import compare_mbins, failed_files
 
 
 DATA_PATH = op.join(op.dirname(__file__), 'data')
-BASE_PATH = op.join("Build", "Release", "win-x64")
 TO_EXML_FAIL = 'Failed conversion to EXML'
 TO_MBIN_FAIL = 'Failed conversion to MBIN'
 
@@ -15,40 +14,38 @@ def pytest_generate_tests(metafunc):
     """ Generate a parameterization that contains the list of all the files
     that need to be tested. Ie. every file in the ./data folder.
     """
-    _datapath = metafunc.config.getoption('datapath') or DATA_PATH
+    datapath = os.environ.get('datapath', DATA_PATH)
     # If we want to only rerun the failed tests, return only that list
     if metafunc.config.getoption('rerun') == 'failed':
-        fpaths = failed_files(_datapath)
+        fpaths = failed_files(datapath)
     else:
         # Otherwise, collect the file list as normal
         fpaths = list()
-        for root, _, files in os.walk(_datapath):
+        for root, _, files in os.walk(datapath):
             for fname in files:
-                if op.splitext(fname)[1] == '.MBIN':
-                    fpaths.append(op.join(root, fname))
+                fname_, ext = op.splitext(fname)
+                if ext == '.MBIN':
+                    fpaths.append(op.relpath(op.join(root, fname_), datapath))
     metafunc.parametrize("fname", fpaths)
 
 
-@pytest.fixture()
-def MBINCompiler(platform):
-    if platform == 'linux-x64':
-        # need to run with mono on linux
-        # Build path also includes platform on the CI
-        cmd = ['sudo', 'mono',
-               op.join(BASE_PATH, platform, 'MBINCompiler.exe'), '-q']
-    else:
-        cmd = [op.join(BASE_PATH, 'MBINCompiler.exe'), '-q']
-    return cmd
-
-
-def test_compare(datapath, MBINCompiler, fname):
+def test_compare(convert_files, fname):
     """ Run the comparison test on a file.
     This test is parameterised by fpath which will contain the paths of all
     .MBIN files in the ./data directory.
     """
-    converted = convert_mbin(fname, MBINCompiler)
-    if converted not in (TO_EXML_FAIL, TO_MBIN_FAIL):
-        assert compare_mbins(fname, converted)
-    else:
-        print(f'{fname},{converted}')
+    converted_exml_dir, converted_mbin_dir = convert_files
+    converted_exml = op.join(converted_exml_dir, fname + '.EXML')
+    converted_mbin = op.join(converted_mbin_dir, fname + '.MBIN')
+    if not op.exists(converted_exml):
+        # If the .EXML file doesn't exist, fail.
+        print(f'{fname},{TO_EXML_FAIL}')
         pytest.fail()
+    elif not op.exists(converted_mbin):
+        # If the .MBIN file doesn't exist, fail.
+        print(f'{fname},{TO_MBIN_FAIL}')
+        pytest.fail()
+    else:
+        # Otherwise, do a comparison of the original and final .MBIN files
+        datapath = os.environ.get('datapath', DATA_PATH)
+        assert compare_mbins(op.join(datapath, fname + '.MBIN'), converted_mbin)
